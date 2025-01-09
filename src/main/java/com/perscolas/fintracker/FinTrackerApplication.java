dec3e5d38f8355eed4d1a4b749bf8565c7b0fddb
Oleksandra Hulchuk <dd221285gaa@gmail.com>
2025-01-09 11:43:49 -0600

    readme updated

--- src/main/java/com/perscolas/fintracker/FinTrackerApplication.java
+++ src/main/java/com/perscolas/fintracker/FinTrackerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FintrackerApplication {
+public class FinTrackerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FintrackerApplication.class, args);
+		SpringApplication.run(FinTrackerApplication.class, args);
 	}
 
 }
--- src/test/java/com/perscolas/fintracker/FinTrackerApplicationTests.java
+++ src/test/java/com/perscolas/fintracker/FinTrackerApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FintrackerApplicationTests {
+class FinTrackerApplicationTests {
 
 	@Test
 	void contextLoads() {
